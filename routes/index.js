49976ded5ffe48de458830edfdad92f470ff29d6
Daniel Alabi <alabid@carleton.edu>
2012-07-21 14:05:57 -0400

    commiting stuff

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,16 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+
+/**
+ * registerUser -
+ * registers the user in our database
+ */
+exports.registerUser = function(req, res) {
+    // use req.body for post, req.param for get
+    var userid;
+
+    userid = req.body.userid;
+
+};
