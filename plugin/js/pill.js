5a2cf4015aed3ddf412b9f0b176358a00c8e1f74
Phillip Quiza <aznwhtey@gmail.com>
2012-07-21 15:32:50 -0400

    Update

--- plugin/js/pill.js
+++ plugin/js/pill.js
@@ -1,20 +1,31 @@
 (function () {
     pill = this;
 
-    var server = "http://late-dawn-8160.herokuapp.com";
+    var server = "http://late-dawn-8160.herokuapp.com/";
     
     pill.init = function() {
 	window.onbeforeunload = function() {
 	    pill.disconnect();
 	};
+	pill.connect();
     };
 
     pill.connect = function() {
+	pill.socket = io.connect(server); 
+	pill.socket.on('message', function(msg) {
+	    console.log(msg); 
+	});
+	pill.socket.on('connect', function(msg) {
+
+	});
+	pill.socket.onclose = function () {
 	
+	};
     };
     
     pill.disconnect = function() {
-
+	pill.send(JSON.stringify(
+	pill.socket.disconnect();
     };
 
     pill.send = function (msg) {
