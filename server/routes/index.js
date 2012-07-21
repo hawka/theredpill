45bab872d95bf65900b6a9630fc0b788e4b0782e
Daniel Alabi <alabid@carleton.edu>
2012-07-21 15:30:22 -0400

    done with register

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,9 @@ var mongoose = require("mongoose")
 , Schema = mongoose.Schema
 , app = require("./app")
 , io = require("socket.io").listen(app);
+, http = require("http");
+, https = require("https");
+
 
 require("./models");
 
@@ -25,8 +28,9 @@ exports.storeInfo= function(req,res){
 exports.registerUser = function(req, res) {
     // use req.body for post, req.param for get
     var userid
-    , response = {infos: [], errors: [], code: 400}
-    , newUser;
+    , response = {code:null}
+    , newUser
+    , options;
 
     // get the user's id
     userid = req.body.userid;
@@ -35,19 +39,53 @@ exports.registerUser = function(req, res) {
     User.find({fbid: userid}, function(err, users){
 	// if the user  already exists
 	if (users.length > 0) {
-	    response.errors.push("User Already Exists");
+	    response.code = 0;
+	    // 0 -> "User Already Exists"
 	    res.json(response);
 	    return;
 	}
 	// if the doesn't exist yet, create the new user
 	// by first performing a get request to the facebook
 	// graph to get some more information about a user
+	options = {
+	    host: "graph.facebook.com"
+	    , port: 80
+	    , path: "/"+userid
+	};
 	
-	
-	newUser = new User();
-	newUser.fbid = userid;
+	// get json received from open graph facebook
+	/**
+	   received JSON:
+	   {
+	   "id": "100000558164299",
+	   "name": "Daniel Alabi",
+	   "first_name": "Daniel",
+	   "last_name": "Alabi",
+	   "link": "http://www.facebook.com/alabidan",
+	   "username": "alabidan",
+	   "gender": "male",
+	   "locale": "en_US"
+	   }
+	*/
+	getJSON(options, function(statusCode, output){
+	    console.log("statusCode: "+statusCode);
+	    console.log("object received: "+JSON.stringify(output));
+	    if (statusCode == 200) {
+		// create and store a newUser
+		newUser = new User();
 
-	
+		newUser.fbid = output.id;
+		newUser.username = output.username;
+		newUser.name = output.first_name + " " + output.last_name;
+		
+		// save the new user
+		newUser.save();
+
+		response.code = 1;
+		// 1 -> "User successfully created"
+		res.json(response);		
+	    }  
+	});	
     });
 };
 
@@ -56,15 +94,13 @@ exports.registerUser = function(req, res) {
  * Define helper functions here
  * .getJSON
  */
-var http = require("http");
-var https = require("https");
 
 /**
  * getJSON:  REST get request returning JSON object(s)
  * @param options: http options object
  * @param callback: callback to pass the results JSON object(s) back
  */
-exports.getJSON = function(options, onResult)
+var getJSON = exports.getJSON =  function(options, onResult)
 {
     console.log("rest::getJSON");
 
