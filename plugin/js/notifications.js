dc144ec13eeb8c947915285dcbabb5d5074fe390
hawka <amalia.hawkins@gmail.com>
2012-07-21 16:52:13 -0400

    wooo more stuff on plugin side

--- plugin/js/notifications.js
+++ plugin/js/notifications.js
@@ -1,35 +1,41 @@
 notifications = {
 
-    DEFAULT_TIMEOUT = 250, //ms
+    DEFAULT_TIMEOUT: 250, //ms
 
     // Collection of notifications currently on screen
-    notifs = [],
-    num_unread_notif = 0,     
+    notifs: [],
+    num_unread_notif: 0,     
+
+    initialize: function() {
+        if (chrome && chrome.browserAction) {
+            chrome.browserAction.setBadgeBackgroundColor({color : [255, 0, 0, 255]});
+        }   
+    },
 
     // figures out and returns the number of unread notifs
     // the user has. called by updateUnreadNotifCount().
     getUnreadNotifCount: function() {
         // TODO
         return num_unread_notif;
-    }
+    },
 
     // updates the little counter on the badge to how many
     // new notifications the user has (ie. people who have 
     // looked at his/her stuff since last time checked.)
-    function updateUnreadNotifCount() {
+    updateUnreadNotifCount: function() {
         if (chrome && chrome.browserAction) {
             // get how many unread notifications there are
             var self = notifications; // js/notifications.js
             chrome.browserAction.setBadgeText({text : ''});
 
             setTimeout(function() {
-                // ensures we don't show '0'
-                var num = (self.getUnreadNotifCount() || '') + ''; 
-                chrome.browserAction.setBadgeText({text : num});
+                   // ensures we don't show '0'
+                    var num = (self.getUnreadNotifCount() || '') + ''; 
+                    chrome.browserAction.setBadgeText({text : num});
                 },
                 self.DEFAULT_TIMEOUT, 
                 false
-            }
+            );
         }
     } 
 
