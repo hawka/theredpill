f40e9badeeb823007099c607af6b6b697937c8fa
sr527 <sr527@cornell.edu>
2012-07-22 05:16:33 -0400

    all notifications

--- plugin/js/render-notifications.js
+++ plugin/js/render-notifications.js
@@ -5,7 +5,23 @@ $(document).ready(function(){
 
     // then get the notifications via an ajax call
     $.get('http://redpill.herokuapp.com/getnotifications', function(data) {
-	
+        data= JSON.parse(data);
+        for (d in data){
+           //information needed to retrieve from action: viewer_id, timestamp, link
+           var stalker= data['viewer_id'];
+           var time= data['timestamp'];
+           var link= data['link'];
+           var type= (data['view_type'] == 0) ? 'Photo': 'Profile';
+           $.get('http://graph.facebook.com/'+ stalker, function(fb_data){
+                fb_data= JSON.parse(fb_data);
+                var real_name= fb_data["name"];
+               
+                var message= real_name + " looked at your <a href=" + link + ">" + type + "</a> at " + timestamp;
+                $('body').append($('<div>']).html('<div>' + message + '</div>'));
+               });
+
+           
+        }
     });
 });
 
