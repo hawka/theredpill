d48b6878a4c8813dd11b848f2b32bb58b6b0e983
sr527 <sr527@cornell.edu>
2012-07-22 08:23:52 -0400

    ldjfldksjf

--- server/routes/index.js
+++ server/routes/index.js
@@ -86,11 +86,12 @@ io.sockets.on('connection', function(socket){
     */
     socket.on('markSeen', function(data) {
 	Action.find({_id:data._ids}, function(err, toSeens) {
-	    console.log(toSeens);
+	    if (toSeens != undefined){
         toSeens.forEach(function(item, i) {
 		item.seen = true;
 		item.save();
 	    });
+        }
 	});
     });
     
