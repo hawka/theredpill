bcc59694e420172c820c81d3c52604b3e4359a2e
hawka <amalia.hawkins@gmail.com>
2012-07-22 04:10:00 -0400

    minor fix

--- plugin/js/notifications.js
+++ plugin/js/notifications.js
@@ -11,20 +11,16 @@ notifications = {
 
     initialize: function() {
         var self = notifications; 
-    
+   
+        if (chrome && chrome.browserAction) {
+            chrome.browserAction.setBadgeBackgroundColor({color : [255, 0, 0, 255]});
+        }
+ 
         chrome.cookies.get( { url: "https://www.facebook.com" , name: "c_user" } , function(cookie) {
             var curr_uid = cookie.value;
-
-            // try to register user TODO
-
+            // attempt to register user - TODO handle user not logged in case
             $.post( 'http://redpill.herokuapp.com/register?userid=' + curr_uid , function( response ) {
-                
-                // TODO                
-
-                if (chrome && chrome.browserAction) {
-                    chrome.browserAction.setBadgeBackgroundColor({color : [255, 0, 0, 255]});
-                    self.updateUnreadNotifCount();
-                }
+                self.updateUnreadNotifCount();
             });
         });
     },
